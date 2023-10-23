c63cc2107f7f27d2e6d3868b87ad4ad53327b99b
JamalSorani <gamalsalmsorani18@gmail.com>
2023-10-23 23:10:55 +0300

    updated

--- lib/widgets/inf.dart
+++ lib/widgets/inf.dart
@@ -34,105 +34,122 @@ class _InfState extends State<Inf> {
     double x4 = (mediaQuery.size.width - x3) * 0.28;
     double x5 = (mediaQuery.size.width - x4) * 0.1;
     double x6 = (mediaQuery.size.width - x5) * 0.12;
-    return Column(
-      children: [
-        Container(
-          padding: const EdgeInsets.all(10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              SizedBox(
-                width: x1,
-                child: Text(
-                  textAlign: TextAlign.center,
-                  '${widget.id}',
-                  style: const TextStyle(
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-              SizedBox(
-                width: x2,
-                child: Text(
-                  textAlign: TextAlign.center,
-                  widget.name,
-                  style: const TextStyle(
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-              SizedBox(
-                width: x3,
-                child: Text(
-                  textAlign: TextAlign.center,
-                  widget.email,
-                  style: const TextStyle(
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-              SizedBox(
-                width: x4,
-                child: Text(
-                  textAlign: TextAlign.center,
-                  widget.m,
-                  style: const TextStyle(
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-              InkWell(
-                onTap: () {
-                  Navigator.of(context).pushReplacement(MaterialPageRoute(
-                      builder: (context) => EditUserScreen(
-                            id: widget.id,
-                            id1: widget.id1,
-                            name: widget.name,
-                            email: widget.email,
-                            mobile: widget.m,
-                          )));
-                },
-                child: Container(
-                  height: 30,
-                  color: Colors.blue,
-                  width: x6,
-                  child: const Center(
-                    child: Text(
-                      'Edit',
-                      textAlign: TextAlign.center,
+    return Dismissible(
+      key: ValueKey(widget.id1),
+      background: Container(
+        color: Theme.of(context).colorScheme.error,
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 20),
+        child: const Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+      ),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+        deleteUser(widget.id1, context);
+      },
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  width: x1,
+                  child: Text(
+                    textAlign: TextAlign.center,
+                    '${widget.id}',
+                    style: const TextStyle(
+                      color: Colors.black,
                     ),
                   ),
                 ),
-              ),
-              InkWell(
-                onTap: () async {
-                  setState(() {
-                    isLoading = true;
-                  });
-                  await deleteUser(widget.id1, context);
-                  setState(() {
-                    isLoading = false;
-                  });
-                },
-                child: Container(
-                  height: 30,
-                  color: Colors.red,
-                  width: x6,
-                  child: Center(
-                    child: isLoading
-                        ? const CircularProgressIndicator()
-                        : const Text(
-                            'Delete',
-                            textAlign: TextAlign.center,
-                          ),
+                SizedBox(
+                  width: x2,
+                  child: Text(
+                    textAlign: TextAlign.center,
+                    widget.name,
+                    style: const TextStyle(
+                      color: Colors.black,
+                    ),
                   ),
                 ),
-              ),
-            ],
+                SizedBox(
+                  width: x3,
+                  child: Text(
+                    textAlign: TextAlign.center,
+                    widget.email,
+                    style: const TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: x4,
+                  child: Text(
+                    textAlign: TextAlign.center,
+                    widget.m,
+                    style: const TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    Navigator.of(context).pushReplacement(MaterialPageRoute(
+                        builder: (context) => EditUserScreen(
+                              id: widget.id,
+                              id1: widget.id1,
+                              name: widget.name,
+                              email: widget.email,
+                              mobile: widget.m,
+                            )));
+                  },
+                  child: Container(
+                    height: 30,
+                    color: Colors.blue,
+                    width: x6,
+                    child: const Center(
+                      child: Text(
+                        'Edit',
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () async {
+                    setState(() {
+                      isLoading = true;
+                    });
+                    await deleteUser(widget.id1, context);
+                    setState(() {
+                      isLoading = false;
+                    });
+                  },
+                  child: Container(
+                    height: 30,
+                    color: Colors.red,
+                    width: x6,
+                    child: Center(
+                      child: isLoading
+                          ? const CircularProgressIndicator()
+                          : const Text(
+                              'Delete',
+                              textAlign: TextAlign.center,
+                            ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-        const Divider(),
-      ],
+          const Divider(),
+        ],
+      ),
     );
   }
 }
